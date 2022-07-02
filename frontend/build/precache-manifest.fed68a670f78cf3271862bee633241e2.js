1cd22107b8f69a7814d821f6b8d998812376b2a3
simamkele44 <simamkele44@gmail.com>
2022-07-02 08:38:32 +0200

    first commit

---
+++ frontend/build/precache-manifest.fed68a670f78cf3271862bee633241e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc15c9dee34f685d253fa4eebc416eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76c9ec9df5c41cab06c6",
+    "url": "/static/css/2.16d56f53.chunk.css"
+  },
+  {
+    "revision": "de289201f0881e5a8589",
+    "url": "/static/css/main.88b0f899.chunk.css"
+  },
+  {
+    "revision": "76c9ec9df5c41cab06c6",
+    "url": "/static/js/2.5c7c2599.chunk.js"
+  },
+  {
+    "revision": "de289201f0881e5a8589",
+    "url": "/static/js/main.30b8d042.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
